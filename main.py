2bbac1ba18589cd20171e6dde970b6cf7f3fd420
wangkaiming <wangkaiming@forensix.cn>
2023-12-01 11:05:29 +0800

    初始化提交

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import dat_parser
+import txt_parser
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    argv_length = len(sys.argv)
+    if argv_length != 2:
+        print("缺少参数...")
+    elif sys.argv[1] == "-a": # 同时解析dat和txt
+        print("开始解析DATS...")
+        dat_parser.ParseDats()
+        print("开始解析txtS...")
+        txt_parser.ParseTxts()
+    elif sys.argv[1] == "-d":# 只解析dat
+        print("开始解析DATS...")
+        dat_parser.ParseDats()
+    elif sys.argv[1] == "-t":# 只解析txt
+        print("开始解析txtS...")
+        txt_parser.ParseTxts()
